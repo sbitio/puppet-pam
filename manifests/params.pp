737ff7ff802dc79f13fecaaa5f7db383c2e4a0df
Pedro González Serrano <niteman.es@gmail.com>
2014-09-01 21:02:11 +0200

    modernized params

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,13 +1,10 @@
 class pam::params (
-  $conf_d_path = hiera('conf_d_path', '/etc/pam.d'),
-  $access_conf = hiera('access_conf', '/etc/security/access.conf')
+  $conf_d_path = '/etc/pam.d',
+  $access_conf = '/etc/security/access.conf',
 ) {
   case $::operatingsystem {
-    ubuntu, debian: {
-
-    }
-#    redhat, centos: {
-#    }
+    ubuntu, debian: { }
+    redhat, centos: { }
     default: {
       fail("Unsupported platform: ${::operatingsystem}")
     }
